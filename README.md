5dc7afc6f1ef0d790097a5ca7be7bcd728b72d60
mmmatthew <matthew.mdv@gmail.com>
2019-03-05 18:44:58 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # timeseries-degradation
-Degradation of time series with independent and autocorrelated noise
+Degradation of time series with independent and autocorrelated noise.
+
+## To build the package
+
+run `python setup.py bdist_wheel`
+
+## To import the package from github
+
+run `pip install git+https://github.com/mmmatthew/timeseries-degradation`
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+setuptools.setup(
+	name='tsdeg',
+	version='0.1',
+	scripts=['tsdeg.py'],
+	author="Deepak Kumar",
+	author_email="matthew.moydevitry@eawag.ch",
+	description="A tool to degrade time series data",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/mmmatthew/timeseries-degradation",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
