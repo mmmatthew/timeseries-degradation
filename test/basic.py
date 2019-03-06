77a01caf2a01665fece201286082f092778d0316
mmmatthew <matthew.mdv@gmail.com>
2019-03-06 16:30:38 +0100

    scale noise to signal and compute spearman correlation as well

--- test/basic.py
+++ test/basic.py
@@ -1,8 +1,11 @@
 import tsdeg
+import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.DataFrame(dict(vals=[1, 2, 3, 4]))
+data = pd.DataFrame(dict(vals=[1, 2, 30, 40,45,46,48]))
 
-noisy = tsdeg.noisify(data, 'vals', 'normal', 0.2, True)
+noisy, cor = tsdeg.noisify(data, 'vals', 'normal', 0.2, normalize=True)
 
-print(noisy.head())
+noisy.plot()
+plt.show()
+print(cor)
--- tsdeg.py
+++ tsdeg.py
@@ -3,30 +3,35 @@ import os
 from functions import random_iid
 
 
-def noisify(data, signal_col, noise_type, intensity, normalize=False):
-	"""Adds noise to signal
-	:param data: Pandas Dataframe
-	:param signal_col: name of column in which noise should be added
-	:param noise_type: "normal"
-	:param intensity: Standard deviation or equivalent
-	:param normalize: Data column will be normalized before adding noise
-	:return: Dataframe with noisy signal
-	"""
-	if not type(data) == type(pd.DataFrame()):
-		raise Exception('Data is not in pandas DataFrame format')
-	if not signal_col in data.columns:
-		raise Exception('Data does not have column ' + signal_col)
+def noisify(data, signal_col, noise_type, intensity, noisy_signal_col='degraded', normalize=False):
+    """Adds noise to signal
+    :param data: Pandas Dataframe
+    :param signal_col: name of column in which noise should be added
+    :param noise_type: "normal"
+    :param intensity: Standard deviation or equivalent
+    :param normalize: Data column will be normalized before adding noise
+    :return: Dataframe with noisy signal
+    """
+    if not type(data) == type(pd.DataFrame()):
+        raise Exception('Data is not in pandas DataFrame format')
+    if not signal_col in data.columns:
+        raise Exception('Data does not have column ' + signal_col)
+    if noisy_signal_col == signal_col:
+        raise Exception('Columns have identical names')
 
-	df_copy = data.copy()
+    df_copy = data.copy()
 
-	if normalize:
-		df_copy[signal_col] = df_copy[signal_col]/df_copy[signal_col].max()
+    if normalize:
+        df_copy[signal_col] = df_copy[signal_col] / df_copy[signal_col].max()
 
-	if noise_type == 'normal':
-		noise = random_iid.normal(length=df_copy.shape[0], std_dev=intensity)
-	else:
-		raise Exception('noise type not recognized')
+    if noise_type == 'normal':
+        noise = random_iid.normal(length=df_copy.shape[0], std_dev=intensity)
+    else:
+        raise Exception('noise type not recognized')
 
-	df_copy[signal_col] = df_copy[signal_col] + noise
+    # Add noise to signal, scaling to signal size
+    df_copy[noisy_signal_col] = df_copy[signal_col] + df_copy[signal_col]*noise
 
-	return df_copy
+    # Measure correlation
+    correlation = df_copy.corr(method='spearman')
+    return df_copy, correlation[signal_col][noisy_signal_col]
